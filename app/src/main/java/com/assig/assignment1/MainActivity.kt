28583b9d4aa5f3e4045fef552c27d769801c5823
Anmol Dadhwal <dadhwalanmol08@gmail.com>
2024-06-08 11:29:50 +0530

    change

--- app/src/main/java/com/assig/assignment1/MainActivity.kt
+++ app/src/main/java/com/assig/assignment1/MainActivity.kt
@@ -2,8 +2,10 @@ package com.assig.assignment1
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     var number: EditText?= null
     var birthdate: EditText?=null
     var collegename: EditText?=null
+    var yes: RadioButton?= null
+    var no: RadioButton?=null
     var check: Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,29 +28,39 @@ class MainActivity : AppCompatActivity() {
         number=findViewById(R.id.number)
         birthdate=findViewById(R.id.birthdate)
         collegename=findViewById(R.id.collegename)
+        yes=findViewById(R.id.yes)
+        no=findViewById(R.id.no)
         check=findViewById(R.id.check)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        yes?.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                collegename?.visibility = View.VISIBLE
+            } else {
+                collegename?.visibility = View.GONE
+            }
+        }
         check?.setOnClickListener {
             if(name?.text?.trim().isNullOrEmpty()){
                 name?.error="Enter The Name"
             }else if (number?.text?.trim().isNullOrEmpty()){
                 number?.error="Enter The Number"
-            }else if(birthdate?.text?.trim().isNullOrEmpty()){
+            } else if((number?.text?.trim()?.length?:0) <10){
+                number?.error = "Number should be valid"
+            } else if(birthdate?.text?.trim().isNullOrEmpty()){
                 birthdate?.error="Enter The Birth Date"
             }else if (collegename?.text?.trim().isNullOrEmpty()){
                 collegename?.error="Enter The College name"
             } else {
-//                Toast.makeText(
-//                    this,
-//                    "Value Entered ${name?.text} ${number?.text} ${collegename?.text}",
-//                    Toast.LENGTH_SHORT
-//                ).show()
                 var intent = Intent(this, MainActivity2::class.java)
+                intent.putExtra("name", name?.text?.toString()?.trim())
+                intent.putExtra("number",number?.text?.toString()?.trim())
+                intent.putExtra("birthdate",birthdate?.text?.toString()?.trim()?.toInt())
                 startActivity(intent)
             }
+        }
     }
-}}
+}
--- app/src/main/java/com/assig/assignment1/MainActivity2.kt
+++ app/src/main/java/com/assig/assignment1/MainActivity2.kt
@@ -4,23 +4,47 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity2 : AppCompatActivity() {
+    var tvname: TextView?= null
+    var name = ""
+    var tvnumber: TextView?= null
+    var number = ""
+    var tvdob: TextView?= null
+    var dob = ""
+    var tvcollegename: TextView?= null
+    var collegename = ""
     var back: Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
-        back=findViewById(R.id.back)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        intent?.extras?.let {
+            name = it.getString("name") ?: ""
+            number = it.getString("number") ?: ""
+            dob = it.getString("birthdate")?:""
+            collegename= it.getString("collegename")?:""
+            println("name $name number $number birthdate $dob collegename $collegename")
+        }
+        tvname= findViewById(R.id.name)
+        tvname?.setText(name)
+        tvnumber = findViewById(R.id.number)
+        tvnumber?.setText(number)
+        tvdob = findViewById(R.id.birthdate)
+        tvdob?.setText(dob)
+        tvcollegename = findViewById(R.id.collegename)
+        tvcollegename?.setText(collegename)
+        back= findViewById(R.id.back)
         back?.setOnClickListener {
             this.finish()
         }
